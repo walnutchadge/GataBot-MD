e8b68b5af3f8addf07695b684f3a7ce9defdb336
walnutchadge <walnut.chadge@gmail.com>
2026-05-08 22:59:11 -0600

    Actualizar index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import { join, dirname } from 'path'
-import { createRequire } from 'module';
+import { dirname, join } from 'path'
+import { createRequire } from 'module'
 import { fileURLToPath } from 'url'
-import { setupMaster, fork } from 'cluster'
-import { watchFile, unwatchFile } from 'fs'
-import cfonts from 'cfonts';
+import { fork, setupMaster } from 'cluster'
+import { unwatchFile, watchFile } from 'fs'
+import cfonts from 'cfonts'
 import { createInterface } from 'readline'
 import yargs from 'yargs'
 import express from 'express'
@@ -15,8 +15,8 @@ import { promises as fsPromises } from 'fs'
 // https://stackoverflow.com/a/50052194
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const require = createRequire(__dirname) //Incorpora la capacidad de crear el método 'requerir'
-const { name, author } = require(join(__dirname, './package.json')) //https://www.stefanjudis.com/snippets/how-to-import-json-files-in-es-modules-node-js/
-const { say } = cfonts
+const {name, author} = require(join(__dirname, './package.json')) //https://www.stefanjudis.com/snippets/how-to-import-json-files-in-es-modules-node-js/
+const {say} = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
 //const app = express()
@@ -25,11 +25,13 @@ const rl = createInterface(process.stdin, process.stdout)
 say('Kurumi\Aoki\Bot', {
 font: 'chrome',
 align: 'center',
-gradient: ['red', 'magenta']})
-say(`Por WalnutVex`, {
+gradient: ['red', 'magenta']
+})
+say('Powered By WalnutVex', {
 font: 'console',
 align: 'center',
-gradient: ['red', 'magenta']})
+gradient: ['red', 'magenta']
+})
 
 var isRunning = false
 
@@ -52,10 +54,9 @@ font: 'console',
 align: 'center',
 gradient: ['red', 'magenta']
 })
-setupMaster({exec: args[0], args: args.slice(1),
-})
+setupMaster({exec: args[0], args: args.slice(1)})
 let p = fork()
-p.on('message', data => {
+p.on('message', (data) => {
 switch (data) {
 case 'reset':
 p.process.kill()
@@ -65,18 +66,20 @@ break
 case 'uptime':
 p.send(process.uptime())
 break
-}})
+}
+})
 
 p.on('exit', (_, code) => {
 isRunning = false
 console.error('⚠️ ERROR ⚠️ >> ', code)
-start('main.js'); //
+start('main.js') //
 
 if (code === 0) return
 watchFile(args[0], () => {
 unwatchFile(args[0])
 start(file)
-})})
+})
+})
 
 const ramInGB = os.totalmem() / (1024 * 1024 * 1024)
 const freeRamInGB = os.freemem() / (1024 * 1024 * 1024)
@@ -86,28 +89,30 @@ const packageJsonData = await fsPromises.readFile(packageJsonPath, 'utf-8')
 const packageJsonObj = JSON.parse(packageJsonData)
 const currentTime = new Date().toLocaleString()
 let lineM = '⋯ ⋯ ⋯ ⋯ ⋯ ⋯ ⋯ ⋯ ⋯ ⋯ ⋯ 》'
-console.log(chalk.yellow(`╭${lineM}
+console.log(
+chalk.yellow(`╭${lineM}
 ┊${chalk.blueBright('╭┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅')}
 ┊${chalk.blueBright('┊')}${chalk.yellow(`🖥️ ${os.type()}, ${os.release()} - ${os.arch()}`)}
 ┊${chalk.blueBright('┊')}${chalk.yellow(`💾 Total RAM: ${ramInGB.toFixed(2)} GB`)}
 ┊${chalk.blueBright('┊')}${chalk.yellow(`💽 Free RAM: ${freeRamInGB.toFixed(2)} GB`)}
 ┊${chalk.blueBright('╰┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅')}
 ┊${chalk.blueBright('╭┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅')}
-┊${chalk.blueBright('┊')} ${chalk.blue.bold(`🟢INFORMACIÓN :`)}
+┊${chalk.blueBright('┊')} ${chalk.blue.bold('🟢INFORMACIÓN :')}
 ┊${chalk.blueBright('┊')} ${chalk.blueBright('┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅')} 
 ┊${chalk.blueBright('┊')}${chalk.cyan(`💚 Nombre: ${packageJsonObj.name}`)}
 ┊${chalk.blueBright('┊')}${chalk.cyan(`𓃠 Versión: ${packageJsonObj.version}`)}
 ┊${chalk.blueBright('┊')}${chalk.cyan(`💜 Descripción: ${packageJsonObj.description}`)}
-┊${chalk.blueBright('┊')}${chalk.cyan(`😺 Project Author: ${packageJsonObj.author.name} (@gata_dios)`)}
+┊${chalk.blueBright('┊')}${chalk.cyan(`😺 Project Author: ${packageJsonObj.author.name} (@walnut_vex)`)}
 ┊${chalk.blueBright('┊')}${chalk.blueBright('┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅')} 
-┊${chalk.blueBright('┊')}${chalk.yellow(`💜 Colaborador:`)}
-┊${chalk.blueBright('┊')}${chalk.yellow(`• elrebelde21 (Mario ofc)`)}
+┊${chalk.blueBright('┊')}${chalk.yellow('💜 Colaborador:')}
+┊${chalk.blueBright('┊')}${chalk.yellow('• elrebelde21 (Mario ofc)')}
 ┊${chalk.blueBright('╰┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅')} 
 ┊${chalk.blueBright('╭┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅')}
-┊${chalk.blueBright('┊')}${chalk.cyan(`⏰ Hora Actual :`)}
+┊${chalk.blueBright('┊')}${chalk.cyan('⏰ Hora Actual :')}
 ┊${chalk.blueBright('┊')}${chalk.cyan(`${currentTime}`)}
 ┊${chalk.blueBright('╰┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅')} 
-╰${lineM}`));
+╰${lineM}`)
+)
 setInterval(() => {}, 1000)
 } catch (err) {
 console.error(chalk.red(`❌ No se pudo leer el archivo package.json: ${err}`))
@@ -115,8 +120,10 @@ console.error(chalk.red(`❌ No se pudo leer el archivo package.json: ${err}`))
 
 let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
 if (!opts['test'])
-if (!rl.listenerCount()) rl.on('line', line => {
+if (!rl.listenerCount())
+rl.on('line', (line) => {
 p.emit('message', line.trim())
-})}
+})
+}
 
 start('main.js')
