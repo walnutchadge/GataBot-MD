8b77c582b325a4db4681ddb9599134c39e45ae47
walnutchadge <walnut.chadge@gmail.com>
2026-05-08 21:57:34 -0600

    Actualizar index.js

--- index.js
+++ index.js
@@ -22,11 +22,11 @@ const rl = createInterface(process.stdin, process.stdout)
 //const app = express()
 //const port = process.env.PORT || 8080;
 
-say('Gata\nBot\nMD', {
+say('Kurumi\Aoki\Bot', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
-say(`Por GataDios`, {
+say(`Por WalnutVex`, {
 font: 'console',
 align: 'center',
 gradient: ['red', 'magenta']})
